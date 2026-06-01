1ad32f84f41d32809bc06a51303283fd4d3d6da4
Sujal Garg <sujalgarg@Kunals-MacBook-Air-3.local>
2026-06-01 20:12:21 +0530

    create frontend

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,16 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser')
 const connectDB = require('./db/db')
 const userRoutes = require('./routes/user.routes')
 
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
 
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
 
 connectDB();
 
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
+const blacklistTokenModel = require('../models/blacklistToken.model')
 
 module.exports.registerUser = async (req, res, next) =>{
     const errors = validationResult(req);
@@ -44,5 +45,23 @@ module.exports.loginUser = async (req, res, next) =>{
 
     const token = user.generateAuthToken();
 
+    res.cookie('token', token)
+
     res.status(200).json({token, user});
+}
+
+module.exports.getUserProfile = async (req, res, next) =>{
+    // const errors = validationResult(req);
+    
+    res.status(200).json(req.user);
+    // const { firstname, email } = req.body;
+}
+
+
+module.exports.logoutUser = async (req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.header.authorizationsplit(' ') [ 1 ];  
+
+    await blacklistTokenModel.create({token})
+    res.status(200).json({message: "Logged out"})
 }
